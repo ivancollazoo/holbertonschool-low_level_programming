8eada3741ebc0c6001a6f6bb28f4032fb0f250b0
ivancollazoo <collazoivan1412@gmail.com>
2023-10-18 19:46:39 -0400

    0-10

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all single digit numbers of base 10 starting from 0.
+ * main - print all single digit numbers of base 10 starting from 0.
  * Return = 0 (Success)
  *
 */
