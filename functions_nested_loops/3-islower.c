00bad7c45ab656e066d7f30c0bcc3087435656e2
ivancollazoo <collazoivan1412@gmail.com>
2023-10-19 16:29:56 -0400

    Return 1

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,33 +4,18 @@
 /**
  * _islower - Write a function that checks for lowercase character.
  * Return: 1 if charcter is lowercase, 0 otherwise.
- * @c: the character to be checked.
- *
+ * 
 */
 
 
 int _islower(int c) 
- {
-    if (result) {
-        putchar('1');
-    } else {
-        putchar('0');
-    }
+ 
+{
+
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
 }
-
-int _islower(int c) {
-    if (c >= 'a' && c <= 'z') {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-int main() {
-    char character = 'a';
-    int result = _islower(character);
-    
-    return 0;
-}
-
-
