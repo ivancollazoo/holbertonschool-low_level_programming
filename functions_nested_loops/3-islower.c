257d169fdef3762358280b313dc32e91fc2db537
ivancollazoo <collazoivan1412@gmail.com>
2023-10-19 18:05:42 -0400

    Return 1

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
 /**
  * _islower - Write a function that checks for lowercase character.
  * Return: 1 if charcter is lowercase, 0 otherwise.
- * 
+ * @c: is a letter, lowercase or uppercase. 
 */
 
 
