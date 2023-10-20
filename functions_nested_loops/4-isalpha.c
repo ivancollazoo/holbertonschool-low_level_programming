84b19b0ebbeec3c9edb55c339d0e101c37aa0115
ivancollazoo <collazoivan1412@gmail.com>
2023-10-20 09:10:21 -0400

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Write a function that checks for alphabetic character.
+ *
+ */
+
+int _isalpha(int c)
+
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
