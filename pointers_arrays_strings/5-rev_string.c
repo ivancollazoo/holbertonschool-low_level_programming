3fe4d5a60fa01b3b693d752f4fbff3b4aa604aeb
ivancollazoo <collazoivan1412@gmail.com>
2023-10-25 18:34:43 -0400

    pointers

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,24 +7,17 @@
 
 void rev_string(char *s)
 {
-	char tmp;
-	int i, len, len1;
+	   int length = 0;
+	int i, j;
 
-	len = 0;
-	len1 = 0;
+    while (s[length] != '\0') {
+        length++;
+    }
 
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	len = len - 1;
-
-	for (i = 0; i < len / 2; i++)
-	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
-	}
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
 
 }
