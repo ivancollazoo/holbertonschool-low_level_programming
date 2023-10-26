b84397ada6e9d51d55776436823d52a50cadd351
ivancollazoo <collazoivan1412@gmail.com>
2023-10-25 21:10:51 -0400

    pointers

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,16 @@
 
 void puts2(char *str)
 {
-	int len, i;
+	int len, i;                          //two interger variables, len and i are declared, and len is initialized to 0.
+
 
 	len = 0;
 
-	while (str[len] != '\0')
+	while (str[len] != '\0')             /**This while loop calculates the length of the input string str.
+										  * It iterates through the characters in the string until it finds the null terminator '\0',
+										  * which marks the end of the string. The loop increments the len variable with each character,
+										  * effectively counting the number of characters in the string.
+	                                      */
 	{
 	len++;
 	}
