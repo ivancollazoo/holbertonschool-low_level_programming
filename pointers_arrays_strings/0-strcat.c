d2ec5e6a3f46a52c7a2dd8ab5a99c9c4f6154196
ivancollazoo <collazoivan1412@gmail.com>
2023-10-29 16:02:03 -0400

    pointers

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * *_strcat - Write a function that concatenates two strings.\
- * @src: a string.
- * @dest: string.
+ * *_strcat - Write a function that concatenates two strings.
+ * @dest: string to append to.
+ * @src: string to add.
  *
- * Return: always dest.
+ * Return: a pointer to the resulting string.
  */
-
 
 char *_strcat(char *dest, char *src)
 {
