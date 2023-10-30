ae9ae626e5e5ada2ce8b4daffc0e5c87388e908d
ivancollazoo <collazoivan1412@gmail.com>
2023-10-29 20:26:49 -0400

    pointers

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
