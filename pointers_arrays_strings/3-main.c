51f8fe7948faef51f1220dc41659eee5a43c7029
ivancollazoo <collazoivan1412@gmail.com>
2023-10-29 20:22:19 -0400

    pointers

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -2,25 +2,17 @@
 #include <stdio.h>
 
 /**
- * print_array - print an array of integers
- * @a: an array of integers
- * @n: the number of elements to swap
+ * main - check the code
  *
- * Return: nothing.
+ * Return: Always 0.
  */
-void print_array(int *a, int n)
+int main(void)
 {
-    int i;
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
 
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
 }
